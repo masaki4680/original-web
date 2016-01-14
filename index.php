f622bd63e2ddc77c31cb11be327295281f9ec1e8
Masaki Nakamura <masaki46803980@gmail.com>
2016-01-14 17:58:02 +0900

    １月１４日

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h3>スクイズ</h3>
+<form action="./search_result.php" method="POST">
+<input type="search" name="search" placeholder="キーワードを入力してください">
+<input type="submit" name="submit" value="検索する">
+</form>
+</body>
+</html>
